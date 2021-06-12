1d44202c5cd61233cbd46b2dee403a4d2f1c9517
tito85 <eduardo.flores@fing.edu.uy>
2021-06-11 21:02:25 -0300

    Barra de inicio
    Se borra la palabra Navbar para que solo quede el  inico y favoritos

--- src/components/PersonajeInfoCarac.jsx
+++ src/components/PersonajeInfoCarac.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
-//import Card from 'react-bootstrap/Card'
 
 
 function PersonajeInfoCarac({isLoading,informacion}) {
--- src/components/navegacion/NavBar.jsx
+++ src/components/navegacion/NavBar.jsx
@@ -7,12 +7,12 @@ function NavBar({search}) {
     const onSearch= (q)=>{
         setText(q)
         search(q)
-    }
+    }//
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand"></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
