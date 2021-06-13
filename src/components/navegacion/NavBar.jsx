afd32ea63f4ea52b32e9196b34e03f37c406a649
Antony <antonyh891@gmail.com>
2021-06-13 16:07:09 -0300

    fix navbar

--- src/components/navegacion/NavBar.jsx
+++ src/components/navegacion/NavBar.jsx
@@ -1,40 +1,29 @@
 import React, {useState, useContext} from 'react'
 import {Link} from 'react-router-dom'
 import { FavoritosStateContext } from '../../FavoritosContext'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
 
 
 function NavBar() {
-   
-const listadoFavoritos= useContext(FavoritosStateContext) 
+    
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
-                <div className="container-fluid">
-                    <a className="navbar-brand"></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                            <Link to="/"> <a className="nav-link" aria-current="page" >Inicio</a></Link>
-                            </li>
-                            <li className="nav-item">
-                            <Link to={{
-                                        pathname: `/favoritos`,
-                                        state: {
-                                        listadoFavoritos : [listadoFavoritos]
-                                        }, 
-                                     }}> <a className="nav-link" aria-current="page" >Favoritos</a></Link>
-                            </li>
-                        </ul>
-                        </div>
-                        </div>
-                       
-                    
-                
-            </nav>
-        </div>
+        
+        <>
+         
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+    <Link to to="/"><a className="nav-link" aria-current="page">Inicio</a></Link>
+    <Link to to="/favoritos"><a className="nav-link" aria-current="page">Favoritos</a></Link>
+    </Nav>
+    
+  </Navbar>  
+</>
+        
     )
 }
 
