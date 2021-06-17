255fa2a0d729aee4f2f0b649e941dda50262ecab
Antony <antonyh891@gmail.com>
2021-06-17 02:24:05 -0300

    v

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,7 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 ReactDOM.render(
   <React.StrictMode>
