ad7c809edd736555832b98a65ca79f1f85ceb166
Antony <antonyh891@gmail.com>
2021-06-18 16:11:34 -0300

    v

--- src/setupProxy.jsx
+++ src/setupProxy.jsx
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    'api/',
+    'https://superheroapi.com/',
     createProxyMiddleware({
       target: 'https://superheroapi.com',
       changeOrigin: true,
